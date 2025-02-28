873d1c3e2d68503433ce6e21ba4392d02e5f37d6
Francis Jacob <dev.francisjacob@gmail.com>
2025-02-28 10:13:46 +0100

    made changes

--- src/App.js
+++ src/App.js
@@ -56,19 +56,6 @@ function TodoList() {
             </span>
           </div>
         </li>
-
-        <li>
-          <div>
-            <span>
-              <i className="icon fa-regular fa-circle"></i>
-              <p>Go to the supermarket</p>
-            </span>
-            <span>
-              <i className="icon pen-icon fa-solid fa-pen-fancy"></i>
-              <i className="icon trash-icon fa-regular fa-trash-can"></i>
-            </span>
-          </div>
-        </li>
       </ul>
     </div>
   );

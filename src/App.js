5dac273e1ec94929dff77ace4e7607f7e443fbfa
Francis Jacob <dev.francisjacob@gmail.com>
2025-03-10 11:18:28 +0100

    added functionality. delete task feature etc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
+// const todos = [
+//   { description: "Go to the Gym", completed: false },
+//   { description: "Code Crunch", completed: false },
+//   { description: "Go to the supermarket", completed: false },
+// ];
+
 export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newInput, setNewInput] = useState("");
+
   return (
     <div className="container">
       <Header />
-      <SubmitInput />
-      <TodoList />
+      <SubmitInput
+        newInput={newInput}
+        setNewInput={setNewInput}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      />
+      <List todoList={todoList} setTodoList={setTodoList} />
     </div>
   );
 }
@@ -13,75 +29,90 @@ function Header() {
     <div>
       <i className="logo fa-solid fa-dice-d20"></i>
       <h1>Todo List App</h1>
-      <p>Organize Your Day, One Task at a Time</p>
+      <p>Organize Your Day, One Task at a Time.</p>
     </div>
   );
 }
 
-function SubmitInput() {
+function SubmitInput({ setNewInput, newInput, todoList, setTodoList }) {
+  function storeInput(e) {
+    setNewInput(e.target.value);
+  }
+
+  function addNewTask(e) {
+    e.preventDefault();
+    if (newInput.trim() === "") {
+      setNewInput("");
+      return;
+    }
+    const exists = todoList.some((todo) => todo.description === newInput);
+
+    if (exists) {
+      alert("Task already Exists");
+    } else {
+      setTodoList((prev) => {
+        return [...todoList, { description: newInput, completed: false }];
+      });
+      setNewInput("");
+    }
+  }
+
   return (
-    <form className="submit-input">
-      <input placeholder="Enter Task Here" />
+    <form className="submit-input" onSubmit={addNewTask}>
+      <input
+        value={newInput}
+        onChange={storeInput}
+        placeholder="Enter Task Here"
+      />
       <button className="submit-btn">Add Task</button>
     </form>
   );
 }
 
-function TodoList() {
+function List({ todoList, setTodoList }) {
+  function handleDeleteTask(todoToRemove) {
+    setTodoList(todoList.filter((todo) => todo !== todoToRemove));
+  }
+
+  function handleCompletedTask(todoToCheck) {
+    setTodoList((prev) =>
+      prev.map((todo) =>
+        todo === todoToCheck ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
   return (
     <div className="list-container">
       <ul>
-        <li>
-          <div>
-            <span>
-              <i className="icon fa-regular fa-circle"></i>
-              <p>Go to the Gym</p>
-            </span>
-            <span>
-              <i className="icon pen-icon fa-solid fa-pen-fancy"></i>
-              <i className="icon trash-icon fa-regular fa-trash-can"></i>
-            </span>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <span>
-              <i className="icon fa-regular fa-circle"></i>
-              <p>Code Crunch</p>
-            </span>
-            <span>
-              <i className="icon pen-icon fa-solid fa-pen-fancy"></i>
-              <i className="icon trash-icon fa-regular fa-trash-can"></i>
-            </span>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <span>
-              <i className="icon fa-regular fa-circle"></i>
-              <p>Check Crypto Portfolio</p>
-            </span>
-            <span>
-              <i className="icon pen-icon fa-solid fa-pen-fancy"></i>
-              <i className="icon trash-icon fa-regular fa-trash-can"></i>
-            </span>
-          </div>
-        </li>
-
-        <li>
-          <div>
-            <span>
-              <i className="icon fa-regular fa-circle"></i>
-              <p>Go to the supermarket</p>
-            </span>
-            <span>
-              <i className="icon pen-icon fa-solid fa-pen-fancy"></i>
-              <i className="icon trash-icon fa-regular fa-trash-can"></i>
-            </span>
-          </div>
-        </li>
+        {todoList.map((todo, index) => {
+          return (
+            <div key={todo.description}>
+              <li>
+                <span>
+                  <i
+                    className={`icon ${
+                      todo.completed
+                        ? "fa-solid fa-circle-check"
+                        : "fa-regular fa-circle"
+                    }`}
+                    onClick={() => handleCompletedTask(todo)}
+                  ></i>
+                  <p className={`${todo.completed && "completed"}`}>
+                    {todo.description}
+                  </p>
+                </span>
+                <span>
+                  <i className="icon pen-icon fa-solid fa-pen-fancy"></i>
+                  <i
+                    className="icon trash-icon fa-regular fa-trash-can"
+                    onClick={() => handleDeleteTask(todo)}
+                  ></i>
+                </span>
+              </li>
+            </div>
+          );
+        })}
       </ul>
     </div>
   );
